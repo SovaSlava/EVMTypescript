692b98fdde32a24e50c4c44f512ce16bd655a530
Viacheslav <slavahustle@gmail.com>
2022-11-24 23:32:02 +0300

    signextend - remove old method

--- typescript/helpers/SIGNEXTEND.ts
+++ typescript/helpers/SIGNEXTEND.ts
@@ -2,11 +2,11 @@ export function SIGNEXTEND(stack: bigint[]): bigint[] {
     let signextend: bigint;
     let signedN = BigInt.asIntN(8, BigInt(stack[1]));
     let sign = Math.sign(Number(signedN.toString().slice(0, 2))) == -1 ? 1 : 0;
-    let m = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
-    let o = 0x0000000000000000000000000000000000000000000000000000000000000000;
+    // let m = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+    // let o = 0x0000000000000000000000000000000000000000000000000000000000000000;
     let res;
     if (sign == 1) {
-        let res1 = (BigInt(m) << BigInt(8) | stack[1]).toString(16);
+        // let res1 = (BigInt(m) << BigInt(8) | stack[1]).toString(16);
         //res = res1.slice(res1.length - 32, res1.length)
     }
     else {
