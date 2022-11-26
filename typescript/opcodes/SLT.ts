f04b64db4dee7316e0e842403830eb2b9eb23bb7
Viacheslav <slavahustle@gmail.com>
2022-11-26 21:53:44 +0300

    SLT

---
+++ typescript/opcodes/SLT.ts
@@ -0,0 +1,17 @@
+export function SLT(stack: bigint[]): bigint[] {
+    let slt: bigint;
+    const size0: number = Math.ceil(stack[0].toString(2).length / 8) * 8;
+    const size1: number = Math.ceil(stack[1].toString(2).length / 8) * 8;
+    const value0: bigint = BigInt.asIntN(size0, BigInt(stack[0]));
+    const value1: bigint = BigInt.asIntN(size1, BigInt(stack[1]));
+    if (value0 < value1) {
+        slt = BigInt(1);
+    }
+    else {
+        slt = BigInt(0);
+    }
+    stack.shift();
+    stack.shift();
+    stack.push(slt);
+    return stack;
+}
