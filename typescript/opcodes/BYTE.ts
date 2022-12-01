bbc63415f4ee288bbc9733e447881f7f10266f06
Viacheslav <slavahustle@gmail.com>
2022-12-01 21:06:33 +0300

    BYTE final

---
+++ typescript/opcodes/BYTE.ts
@@ -0,0 +1,19 @@
+export function BYTE(stack: bigint[]): bigint[] {
+    let max = BigInt(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff)
+    let byte: bigint;
+    if (stack[0] > BigInt(32)) {
+        byte = BigInt(0);
+    }
+    else {
+        let value1: string = stack[1].toString(16);
+        while (value1.length != 64) {
+            value1 = '0' + value1;
+        }
+        let substrPlace = Number(stack[0] << BigInt(1));
+        byte = BigInt(`0x${value1.substring(substrPlace, substrPlace + 2)}`)
+    }
+    stack.shift();
+    stack.shift();
+    stack.push(byte);
+    return stack;
+}
