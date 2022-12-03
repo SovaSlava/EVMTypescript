269979dae24d33e3eaa3ea5ae3a6bd90e8386f10
Viacheslav <slavahustle@gmail.com>
2022-12-03 21:06:13 +0300

    JUMPDEST

--- typescript/evm.ts
+++ typescript/evm.ts
@@ -118,7 +118,8 @@ export default function evm(code: Uint8Array) {
       case 0xFE: success = opcodes.INVALID(); break;
       case 0x58: stack = opcodes.PC(pc, stack); break;
       case 0x5A: stack = opcodes.GAS(stack); break;
-      case 0x56: pc = opcodes.JUMP(pc, stack); break;
+      case 0x56: [pc, stack, success] = opcodes.JUMP(pc, stack, code); break;
+      case 0x5B: opcodes.JUMPDEST(); break;
     }
 
 
--- typescript/opcodes/JUMP.ts
+++ typescript/opcodes/JUMP.ts
@@ -1,4 +1,27 @@
-export function JUMP(pc: number, stack: bigint[]): number {
-    pc = Number(stack[0]);
-    return pc;
+export function JUMP(pc: number, stack: bigint[], code: string): [number, bigint[], boolean] {
+    let success: boolean = true;
+    if (code[Number(stack[0])].toString(16) == '5b' && Number(stack[0]) < code.length) {
+        for (let i = Number(stack[0]) - 1; i >= 0; i--) {
+            if (Number(code[i]) > 0x5F && Number(code[i]) < 0x80) {
+                let argSize: number = Number(code[i]) - 0x60 + 1;
+                if ((i + argSize) == Number(stack[0])) {
+                    console.log('basaad')
+                    success = false;
+                    break;
+                }
+            }
+        }
+        if (success) {
+            pc = Number(stack[0]);
+            stack.shift();
+        }
+    }
+    else {
+        success = false;
+    }
+    if (success != true) {
+        stack = []
+        pc = code.length;
+    }
+    return [pc, stack, success];
 }
