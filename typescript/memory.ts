0adb83e7fcc2ce792e670a13c0a249ab901b6533
Viacheslav <slavahustle@gmail.com>
2022-12-04 17:20:10 +0300

    memory v1

---
+++ typescript/memory.ts
@@ -0,0 +1,22 @@
+export class Memory {
+
+    private data: Uint8Array;
+
+    constructor() {
+        this.data = new Uint8Array(1024 * 1024);
+    }
+
+    store(offset: bigint, value) {
+        for (let i = 0n; i < 32n; i++) {
+            this.data[Number(offset + i)] = Number((value >> ((32n - i - 1n) * 8n)) & 0xffn);
+        }
+    }
+
+    load(offset: bigint) {
+        let value = 0n;
+        for (let i = 0n; i < 32n; i++) {
+            value = (value << 8n) | BigInt(this.data[Number(offset + i)]);
+        }
+        return value;
+    }
+}
